306581ec8415dd13bef3a7db87fa3dc8aa5e049c
Jared Chamberlain <jchamberlain909@gmail.com>
2018-04-22 01:41:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,3 +28,12 @@ function destructivelyRemoveElementFromBeginningOfArray(arr){
 function removeElementFromBeginningOfArray(arr){
   return arr.slice(1);
 }
+
+function destructivelyRemoveElementFromEndOfArray(arr){
+  arr.pop();
+  return arr;
+}
+
+function removeElementFromEndOfArray(arr){
+  return arr.slice(0,arr.length-1);
+}
