8c7a51910aab32f1a367ad13c332b1cac37b26e1
Jared Chamberlain <jchamberlain909@gmail.com>
2018-04-22 01:34:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,3 +6,12 @@ function destructivelyAddElementToBeginningOfArray (arr, e) {
   arr.unshift(e);
   return arr;
 }
+
+function addElementToBeginningOfArray (arr, e) {
+  return [e, ...arr];
+}
+
+function destructivelyAddElementToBeginningOfArray (arr, e) {
+  arr.unshift(e);
+  return arr;
+}
